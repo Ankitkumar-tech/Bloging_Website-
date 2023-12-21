22c2b04ee78e4067fbe8a4f9a8e9a925987f037b
Ankitkumar-tech <ankit8287670719@gmail.com>
2023-12-21 17:09:06 +0530

    created search dialog , footer

--- bloging/src/Pages/home/Home.jsx
+++ bloging/src/Pages/home/Home.jsx
@@ -1,11 +1,13 @@
 import React from "react";
 import Layout from "../../Components/layout/Layout";
 import HeroSection from "../../Components//heroSection/HeroSection";
+import BlogPostCard from "../../Components/blogPostcard/BlogPostCard";
 
 function Home() {
   return (
     <Layout>
       <HeroSection />
+      <BlogPostCard />
     </Layout>
   );
 }
