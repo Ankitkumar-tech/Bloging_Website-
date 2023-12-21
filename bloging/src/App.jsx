b32a7a746c5b35982d6b65b5c9d5a3f876416e9c
Ankitkumar-tech <ankit8287670719@gmail.com>
2023-12-21 15:35:50 +0530

    route setup done for all the file

--- bloging/src/App.jsx
+++ bloging/src/App.jsx
@@ -1,18 +1,34 @@
-// import { useState } from "react";
+import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
+// import Home from "./pages/home/Home";
+// import Blog from "./pages/blog/Blog";
+// import AllBlogs from "./pages/allBlogs/AllBlogs";
+// import NoPage from "./pages/nopage/NoPage";
+// import BlogInfo from "./pages/blogInfo/BlogInfo";
+// import AdminLogin from "./pages/admin/adminLogin/AdminLogin";
+// import Dashboard from "./pages/admin/dashboard/Dashboard";
+import Home from "./Pages/home/Home";
+import Blog from "./Pages/blog/blog";
+import AllBlogs from "./Pages/allBlogs/AllBlogs";
+import BlogInfo from "./Pages/blogInfo/BlogInfo";
+import AdminLogin from "./Pages/admin/adminLogin/AdminLogin";
+import Dashboard from "./Pages/admin/dashboard/Dashboard";
+import NoPage from "./Pages/noPage/NoPage";
 
-import { Button } from "@material-tailwind/react";
-// import React from "react";
 function App() {
-  // const [count, setCount] = useState(0);
-
   return (
-    <>
-      <div className="flex justify-center  items-center h-screen">
-        <Button>Click</Button>
-      </div>
-
-      <h1>Hello world</h1>
-    </>
+    <div>
+      <Router>
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/blog" element={<Blog />} />
+          <Route path="/allblogs" element={<AllBlogs />} />
+          <Route path="/bloginfo/:id" element={<BlogInfo />} />
+          <Route path="/adminlogin" element={<AdminLogin />} />
+          <Route path="/dashboard" element={<Dashboard />} />
+          <Route path="/*" element={<NoPage />} />
+        </Routes>
+      </Router>
+    </div>
   );
 }
 
