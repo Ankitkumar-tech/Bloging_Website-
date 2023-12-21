6a8f55b1c363821a37b92a95066aeb03643e7b6f
Ankitkumar-tech <ankit8287670719@gmail.com>
2023-12-21 15:29:42 +0530

    project setup done

---
+++ bloging/src/App.jsx
@@ -0,0 +1,19 @@
+// import { useState } from "react";
+
+import { Button } from "@material-tailwind/react";
+// import React from "react";
+function App() {
+  // const [count, setCount] = useState(0);
+
+  return (
+    <>
+      <div className="flex justify-center  items-center h-screen">
+        <Button>Click</Button>
+      </div>
+
+      <h1>Hello world</h1>
+    </>
+  );
+}
+
+export default App;
